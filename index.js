31eabb8dec432b39a76a41e4a0716d87799bd084
JeixK <166679558+JeixK@users.noreply.github.com>
2024-04-10 21:43:34 -0700

    Update index.js

--- index.js
+++ index.js
@@ -23,33 +23,24 @@ client.on('ready', async () => {
   console.log(`${client.user.tag} - rich presence started!`);
 
   const r = new Discord.RichPresence()
-    .setApplicationId('')
+    .setApplicationId('1227824143035138048')
     .setType('STREAMING')
-    .setURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ') //Must be a youtube video link 
-    .setState('Recording')
-    .setName('')
-    .setDetails(`Valorant [${formatTime()}]`)
-    .setStartTimestamp(Date.now())
- .setAssetsLargeImage('') //You can put links in tenor or discord and etc.
-    .setAssetsLargeText('Boring') //Text when you hover the Large image
-    .setAssetsSmallImage('') //You can put links in tenor or discord and etc.
-    .setAssetsSmallText('Twitch') //Text when you hover the Small image
-    .addButton('Watch', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-    .addButton('Donate', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
-
+    .setURL('https://discord.com/channels/@me/1103492118913224785/1227830311862272032') //Must be a youtube video link 
+    .setState('LeVaneEmpire')
+    .setName('Jei')
+    .setDetails(`Luna is Cute`)
+ .setAssetsLargeImage('https://media.discordapp.net/attachments/1103492118913224785/1227831994264588388/5210BA8D-D7FD-4E08-97E3-3E5E382BC6D9.gif?ex=6629d6a7&is=661761a7&hm=40709eb5fe274354e98f4947210cf0fac6e58dd44e252f811841f889b6300cee&=') //You can put links in tenor or discord and etc.
+    .setAssetsLargeText('Baby, you take my breath away.')
+    .setAssetsSmallText('Fuck that nigga Nyx...') //Text when you hover the Small image
+    .addButton('Pain', 'https://open.spotify.com/track/34RdcQ0rkkbdjbx7ks6BJF?si=3135c7e4fc524462')
+    .addButton('Hoyo Noise', 'https://open.spotify.com/track/49xooPdasAQlKhW6Sf1Rhq?si=a7c5656af30e41be');
   client.user.setActivity(r);
   client.user.setPresence({ status: "dnd" }); //dnd, online, idle, offline
 
-  let prevTime = null;
-  setInterval(() => {
-    const newTime = formatTime();
-    if (newTime !== prevTime) {
-      const newDetails = ` [${newTime}]`;
+  
       r.setDetails(newDetails);
       client.user.setActivity(r);
       prevTime = newTime;
-    }
-  }, 1000); // Update every second
 });
 
 const mySecret = process.env['TOKEN'];
